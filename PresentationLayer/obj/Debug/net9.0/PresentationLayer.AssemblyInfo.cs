e92a4630bf801352ae5b691d7c4891f34c22d467
Basmala Ibrahim <102371084+BsmalaIbrahem@users.noreply.github.com>
2025-07-26 22:17:50 +0300

    Repositories

--- PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781ae24fb7e4a8340b4768948d4089ba8da7fcd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
